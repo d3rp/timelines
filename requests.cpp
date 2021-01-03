67dcbf4f0e66771f47cdaf3b9054d1452588084d
Joni Turunen <rojun.itu@example.com>
2021-01-03 19:32:46 +0200

    wip - added json parsing lib. Bypassing sslVerify for now for libcurl/cpr

--- requests.cpp
+++ requests.cpp
@@ -1,16 +1,30 @@
 #include "requests.h"
 #include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
 
 std::string
 request()
 {
-	cpr::Response r = cpr::Get(cpr::Url{ "https://en.wikipedia.org/w/api.php"},
-        cpr::Parameters{
-            {"action", "query"},
-			{"list", "search"},
-			{"srsearch", "platon"},
-			{"format", "json"}
-        });
+    /*
+    auto sslOpts = cpr::Ssl(
+        cpr::ssl::ALPN(false),
+        cpr::ssl::NPN(false),
+        cpr::ssl::MaxTLSVersion(),
+        cpr::ssl::VerifyHost(false),
+        cpr::ssl::VerifyPeer(false),
+        cpr::ssl::VerifyStatus(false)
+    );
+	*/
+    //auto sslOpts = cpr::Ssl(cpr::ssl::ALPN{ false }, cpr::ssl::NPN{ false });
+    cpr::Response r = cpr::Get(cpr::Url{ "https://www.httpbin.org/get" },
+        /*        cpr::Parameters{
+                    {"action", "query"},
+                    {"list", "search"},
+                    {"srsearch", "platon"},
+                    {"format", "json"}
+               },
+               */
+        cpr::VerifySsl(false));
     //cpr::Response r = cpr::Get(cpr::Url{ "https://t.co" });
     //,
     //    cpr::Authentication{ "user", "pass" },
@@ -19,7 +33,9 @@ request()
 //    r.status_code;                  // 200
 //    r.header["content-type"];       // application/json; charset=utf-8
 //    r.text;                         // JSON text string
-    return r.text;
+    auto j = nlohmann::json::parse(r.text);
+    //return j.dump();
+    return j.at("Host");
 }
 
 TEST_CASE("testing curl functionality")
